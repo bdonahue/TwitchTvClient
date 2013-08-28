8cde1803071c8510af1fdc0c20cedeaaeb98ba0e
Bill Donahue <w.donahue@gmail.com>
2013-08-27 21:21:37 -0400

    Getting images to show up in a uniform grid

---
+++ src/com/wdonahue/rapidparsing/views/HeightMatchesWidthImageView.java
@@ -0,0 +1,34 @@
+package com.wdonahue.rapidparsing.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class HeightMatchesWidthImageView extends ImageView {
+	public HeightMatchesWidthImageView(final Context context) {
+		super(context);
+	}
+
+	public HeightMatchesWidthImageView(final Context context,
+			final AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public HeightMatchesWidthImageView(final Context context,
+			final AttributeSet attrs, final int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void onMeasure(final int widthMeasureSpec,
+			final int heightMeasureSpec) {
+		int width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+		setMeasuredDimension(width, width);
+	}
+
+	@Override
+	protected void onSizeChanged(final int w, final int h, final int oldw,
+			final int oldh) {
+		super.onSizeChanged(w, w, oldw, oldh);
+	}
+}
